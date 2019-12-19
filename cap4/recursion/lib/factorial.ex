86bd29ef656aa50929b9b66bc52efaf27985a5b5
Felipe Murakami <felipe.murakami@locaweb.com.br>
2019-12-19 09:45:37 -0300

    Refactor of 'Decrease and Conquer' example

--- cap4/recursion/lib/factorial.ex
+++ cap4/recursion/lib/factorial.ex
@@ -1,7 +1,4 @@
 defmodule Factorial do
   def of(0), do: 1
-  def of(1), do: 1 * of(0)
-  def of(2), do: 2 * of(1)
-  def of(3), do: 3 * of(2)
-  def of(4), do: 4 * of(3)
+  def of(n) when n > 0, do: n * of(n - 1)
 end
