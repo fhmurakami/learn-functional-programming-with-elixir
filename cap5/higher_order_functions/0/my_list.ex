071668896f73890c698f5ed1ce2899752943ee91
Felipe Murakami <felipe.murakami@locaweb.com.br>
2020-01-10 17:11:58 -0300

    Refactor MyList to add Transforming Lists example

--- cap5/higher_order_functions/0/my_list.ex
+++
@@ -1,7 +0,0 @@
-defmodule MyList do
-  def each([], _function), do: nil
-  def each([head | tail], function) do
-    function.(head)
-    each(tail, function)
-  end
-end
---
+++ cap5/higher_order_functions/my_list.ex
@@ -0,0 +1,33 @@
+defmodule MyList do
+  def each([], _function), do: nil
+  def each([head | tail], function) do
+    function.(head)
+    each(tail, function)
+  end
+
+  def map([], _function), do: []
+  def map([head | tail], function) do
+    [function.(head) | map(tail, function)]
+  end
+end
+
+# IEx:
+#
+# enchanted_items = [
+#   %{title: "Edwin's Longsword", price: 150},
+#   %{title: "Healing Potion", price: 60},
+#   %{title: "Edwin's Rope", price: 30},
+#   %{title: "Dragon's Spear", price: 100}
+# ]
+#
+# increase_price = fn item -> update_in(item.price, &(&1 * 1.1)) end
+#
+# MyList.map(enchanted_items, increase_price)
+#
+# Output:
+# [
+#    %{price: 165.0, title: "Edwin's Longsword"},
+#    %{price: 66.0, title: "Healing Potion"},
+#    %{price: 33.0, title: "Edwin's Rope"},
+#    %{price: 110.00000000000001, title: "Dragon's Spear"}
+# ]
