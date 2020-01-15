3d4e33e4cb9a63151d2be8dc393d96ece0333393
Felipe Murakami <felipe.murakami@locaweb.com.br>
2020-01-15 09:31:51 -0300

    Add second example for Chapter 5 - Pipelining Your Functions

---
+++ cap5/higher_order_functions/my_string.ex
@@ -0,0 +1,7 @@
+defmodule MyString do
+  def capitalize_words(title) do
+    words = String.split(title)
+    capitalized_words = Enum.map(words, &String.capitalize/1)
+    Enum.join(capitalized_words, " ")
+  end
+end
