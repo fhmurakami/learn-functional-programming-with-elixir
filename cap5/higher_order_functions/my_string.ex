2016855993968baa82242e07090fbd9cd5623fec
Felipe Murakami <felipe.murakami@locaweb.com.br>
2020-01-15 09:39:10 -0300

    Refactor MyString example to remove unnecessary variables

--- cap5/higher_order_functions/my_string.ex
+++ cap5/higher_order_functions/my_string.ex
@@ -1,7 +1,17 @@
 defmodule MyString do
   def capitalize_words(title) do
-    words = String.split(title)
-    capitalized_words = Enum.map(words, &String.capitalize/1)
-    Enum.join(capitalized_words, " ")
+    Enum.join(
+      Enum.map(
+        String.split(title),
+        &String.capitalize/1
+      ), " "
+    )
   end
 end
+
+# IEx:
+# c("my_string.ex")
+# MyString.capitalize_words("a whole new world")
+#
+# Output:
+# "A Whole New World"
