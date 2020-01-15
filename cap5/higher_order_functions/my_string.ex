799297e48513b3a6f7c97432bb32cb000df5e578
Felipe Murakami <felipe.murakami@locaweb.com.br>
2020-01-15 10:06:00 -0300

    Refactor MyString example to use helper functions

--- cap5/higher_order_functions/my_string.ex
+++ cap5/higher_order_functions/my_string.ex
@@ -2,8 +2,16 @@ defmodule MyString do
   def capitalize_words(title) do
     title
     |> String.split
-    |> Enum.map(&String.capitalize/1)
-    |> Enum.join(" ")
+    |> capitalize_all
+    |> join_with_whitespace
+  end
+
+  def capitalize_all(words) do
+    Enum.map(words, &String.capitalize/1)
+  end
+
+  def join_with_whitespace(words) do
+    Enum.join(words, " ")
   end
 end
 
