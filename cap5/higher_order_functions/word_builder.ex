70b04643983932f428e62e8263e3cf01afc259bb
Felipe Murakami <felipe.murakami@locaweb.com.br>
2020-01-16 09:45:01 -0300

    Refactor WordBuilder to use function-capturing syntax

--- cap5/higher_order_functions/word_builder.ex
+++ cap5/higher_order_functions/word_builder.ex
@@ -1,7 +1,6 @@
 defmodule WordBuilder do
   def build(alphabet, positions) do
-    partial = fn at -> String.at(alphabet, at) end
-    letters = Enum.map(positions, partial)
+    letters = Enum.map(positions, &(String.at(alphabet, &1)))
     Enum.join(letters)
   end
 end
