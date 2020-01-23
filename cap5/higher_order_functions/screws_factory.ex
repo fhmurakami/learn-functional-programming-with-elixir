f46059f1779daf50c6e88f7cf6d6d3dce775d4eb
Felipe Murakami <felipe.murakami@locaweb.com.br>
2020-01-23 10:54:29 -0300

    Increase speed by processing elements in batches

--- cap5/higher_order_functions/screws_factory.ex
+++ cap5/higher_order_functions/screws_factory.ex
@@ -1,19 +1,21 @@
 defmodule ScrewsFactory do
   def run(pieces) do
     pieces
-    |> Stream.map(&add_thread/1)
-    |> Stream.map(&add_head/1)
+    |> Stream.chunk(50)
+    |> Stream.flat_map(&add_thread/1)
+    |> Stream.chunk(100)
+    |> Stream.flat_map(&add_head/1)
     |> Enum.each(&output/1)
   end
 
-  defp add_thread(piece) do
+  defp add_thread(pieces) do
     Process.sleep(50)
-    piece <> "--"
+    Enum.map(pieces, &(&1 <> "--"))
   end
 
-  defp add_head(piece) do
+  defp add_head(pieces) do
     Process.sleep(100)
-    "o" <> piece
+    Enum.map(pieces, &("o" <> &1))
   end
 
   defp output(screw) do
